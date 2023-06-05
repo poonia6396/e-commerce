010140333bc72667ba8b2c76374f237802969504
Nishant Poonia <nishant.poonia6396@gmail.com>
2023-06-05 11:46:10 +0530

    init

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
-
 app.use(cors());
 app.options('*', cors())
 
@@ -21,6 +20,7 @@ const usersRoutes = require('./routes/users');
 const ordersRoutes = require('./routes/orders');
 
 const api = process.env.API_URL;
+console.log(api)
 
 app.use(`${api}/categories`, categoriesRoutes);
 app.use(`${api}/products`, productsRoutes);
@@ -37,6 +37,12 @@ mongoose.connect(process.env.MONGODB_URL, {
 })
 .catch((err)=> {
     console.log(err);
+})
+
+
+app.get(`/`, async (req, res) =>{
+    res.send('Hello!');
+    
 })
 
 //Server
